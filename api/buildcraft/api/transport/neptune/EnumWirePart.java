8fdb96ffc45ab053688ee4a75068f7fc8cb940bb
AlexIIL <alexj9@me.com>
2016-11-06 20:08:45 +0000

    Add a "possible" bounding box for EnumWirePart

--- api/buildcraft/api/transport/neptune/EnumWirePart.java
+++ api/buildcraft/api/transport/neptune/EnumWirePart.java
@@ -2,6 +2,7 @@ package buildcraft.api.transport.neptune;
 
 import net.minecraft.util.EnumFacing.AxisDirection;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
 
 public enum EnumWirePart {
     EAST_UP_SOUTH(true, true, true),
@@ -16,7 +17,12 @@ public enum EnumWirePart {
     public static final EnumWirePart[] VALUES = values();
 
     public final AxisDirection x, y, z;
+
+    /** The bounding box for rendering a wire or selecting an already-placed wire. */
     public final AxisAlignedBB boundingBox;
+
+    /** The bounding box that is used when adding pipe wire to a pipe */
+    public final AxisAlignedBB boundingBoxPossible;
 
     private EnumWirePart(boolean x, boolean y, boolean z) {
         this.x = x ? AxisDirection.POSITIVE : AxisDirection.NEGATIVE;
@@ -29,5 +35,9 @@ public enum EnumWirePart {
         double y2 = this.y.getOffset() * (4 / 16.0) + 0.5;
         double z2 = this.z.getOffset() * (4 / 16.0) + 0.5;
         this.boundingBox = new AxisAlignedBB(x1, y1, z1, x2, y2, z2);
+
+        Vec3d center = new Vec3d(0.5, 0.5, 0.5);
+        Vec3d edge = new Vec3d(x ? 0.75 : 0.25, y ? 0.75 : 0.25, z ? 0.75 : 0.25);
+        this.boundingBoxPossible = new AxisAlignedBB(center, edge);
     }
 }
