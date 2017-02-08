b7112579e8e925abc36727be40d6f1cf0dbc1a72
AlexIIL <alexj9@me.com>
2017-02-08 00:36:06 +0000

    Fix the pipe definition builder defaulting to "buildcrafttransport:pipes/[prefix]" rather than "[active_mod_id]:pipes/[prefix]"

--- api/buildcraft/api/transport/neptune/PipeDefinition.java
+++ api/buildcraft/api/transport/neptune/PipeDefinition.java
@@ -4,6 +4,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModContainer;
 
 public final class PipeDefinition {
     public final ResourceLocation identifier;
@@ -65,8 +66,16 @@ public final class PipeDefinition {
             return id(both).tex(both);
         }
 
+        private static String getActiveModId() {
+            ModContainer mod = Loader.instance().activeModContainer();
+            if (mod == null) {
+                throw new IllegalStateException("Cannot interact with PipeDefinition outside of an activly scoped mod!");
+            }
+            return mod.getModId();
+        }
+
         public PipeDefinitionBuilder id(String post) {
-            identifier = new ResourceLocation(Loader.instance().activeModContainer().getModId(), post);
+            identifier = new ResourceLocation(getActiveModId(), post);
             return this;
         }
 
@@ -74,13 +83,29 @@ public final class PipeDefinition {
             return texPrefix(prefix).texSuffixes(suffixes);
         }
 
+        /** Sets the texture prefix to be: <code>[current_mod_id]:pipes/[prefix]</code> where [current_mod_id] is the
+         * modid of the currently loaded mod, and [prefix] is the string parameter given.
+         * 
+         * @return this */
         public PipeDefinitionBuilder texPrefix(String prefix) {
-            texturePrefix = "buildcrafttransport:pipes/" + prefix;
+            return texPrefixDirect(getActiveModId() + ":pipes/" + prefix);
+        }
+
+        /** Sets the {@link #texturePrefix} to the input string, without any additions or changes (unlike
+         * {@link #texPrefix(String)})
+         * 
+         * @return this */
+        public PipeDefinitionBuilder texPrefixDirect(String prefix) {
+            texturePrefix = prefix;
             return this;
         }
 
+        /** Sets {@link #textureSuffixes} to the given array, or to <code>{""}<code> if the argument list is empty or
+         * null.
+         * 
+         * @return this. */
         public PipeDefinitionBuilder texSuffixes(String... suffixes) {
-            if (suffixes.length == 0) {
+            if (suffixes == null || suffixes.length == 0) {
                 textureSuffixes = new String[] { "" };
             } else {
                 textureSuffixes = suffixes;
