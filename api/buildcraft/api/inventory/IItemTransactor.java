650823c99a9d1d8e5ce297377cdd59a238c68fb4
AlexIIL <alexj9@me.com>
2017-05-10 23:03:29 +0100

    A few default methods for IItemTransactor.

--- api/buildcraft/api/inventory/IItemTransactor.java
+++ api/buildcraft/api/inventory/IItemTransactor.java
@@ -23,8 +23,17 @@ public interface IItemTransactor {
      * @param stacks The stacks to insert. Must not be null!
      * @param simulate If true then the in-world state of this will not be changed.
      * @return The overflow stacks. Will be an empty list if all of it was accepted. */
-    // TODO (Java 8) replace this with a default method that is ItemTransactorHelper.insertAllBypass
-    NonNullList<ItemStack> insert(NonNullList<ItemStack> stacks, boolean simulate);
+    default NonNullList<ItemStack> insert(NonNullList<ItemStack> stacks, boolean simulate) {
+        NonNullList<ItemStack> leftOver = NonNullList.create();
+        for (int i = 0; i < stacks.size(); i++) {
+            ItemStack stack = stacks.get(i);
+            ItemStack leftOverStack = insert(stack, false, simulate);
+            if (!leftOverStack.isEmpty()) {
+                leftOver.add(leftOverStack);
+            }
+        }
+        return leftOver;
+    }
 
     /** Extracts a number of items that match the given filter
      * 
@@ -32,7 +41,23 @@ public interface IItemTransactor {
      * @param min The minimum number of items to extract, or 0 if not enough items can be extracted
      * @param max The maximum number of items to extract.
      * @param simulate If true then the in-world state of this will not be changed.
-     * @return The stack that was extracted, or null if it could not be. */
+     * @return The stack that was extracted, or {@link ItemStack#EMPTY} if it could not be. */
     @Nonnull
     ItemStack extract(@Nullable IStackFilter filter, int min, int max, boolean simulate);
+
+    default boolean canFullyAccept(@Nonnull ItemStack stack) {
+        return insert(stack, true, true).isEmpty();
+    }
+
+    default boolean canPartiallyAccept(@Nonnull ItemStack stack) {
+        return insert(stack, false, true).getCount() < stack.getCount();
+    }
+
+    @FunctionalInterface
+    public interface IItemInsertable extends IItemTransactor {
+        @Override
+        default ItemStack extract(IStackFilter filter, int min, int max, boolean simulate) {
+            return ItemStack.EMPTY;
+        }
+    }
 }
