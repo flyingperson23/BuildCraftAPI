df21a37bca8f5cf07148df5b8f03f0825cefa654
AlexIIL <alexj9@me.com>
2017-09-21 17:59:33 +0100

    Add a simulate paramater to IFlowItems.tryExtractItems.

--- api/buildcraft/api/transport/pipe/IFlowItems.java
+++ api/buildcraft/api/transport/pipe/IFlowItems.java
@@ -11,14 +11,22 @@ import buildcraft.api.core.IStackFilter;
 import buildcraft.api.transport.IInjectable;
 
 public interface IFlowItems extends IInjectable {
+
+    /** @deprecated Use the version below with a simulate paramater. */
+    @Deprecated
+    default int tryExtractItems(int count, EnumFacing from, @Nullable EnumDyeColor colour, IStackFilter filter) {
+        return tryExtractItems(count, from, colour, filter, false);
+    }
+
     /** Attempts to extract items from the inventory connected to this pipe on the given side.
      * 
      * @param count The maximum number of items to extract
      * @param from The direction to extract from.
      * @param colour The colour that extracted items should be painted.
      * @param filter The filter to determine what can be extracted.
+     * @param simulate If true then only simulate the extraction.
      * @return The number of items extracted. */
-    int tryExtractItems(int count, EnumFacing from, @Nullable EnumDyeColor colour, IStackFilter filter);
+    int tryExtractItems(int count, EnumFacing from, @Nullable EnumDyeColor colour, IStackFilter filter, boolean simulate);
 
     /** Inserts an item directly into the centre of this pipe, going in the given direction. This should ONLY be called
      * from an instance of {@link PipeBehaviour}, as otherwise it can lead to problems. (For example crashing if a pipe
