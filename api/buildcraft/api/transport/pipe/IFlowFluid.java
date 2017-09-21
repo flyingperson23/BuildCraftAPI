a2f766271baf2e40d46d07df31edfe0cbb271015
AlexIIL <alexj9@me.com>
2017-09-21 17:30:36 +0100

    Add 2 new methods:
     - IFlowFluid.extractFluidsForce: allows PipeBehaviours or PipePluggables to extract the contents of a fluid pipe directly. Added to make @faceofcats teleport pipes work.
     - IFlowItems.sendPhantomItem: allows PipeBehaviours or PipePluggables to send phantom (fake) items across a pipe. Added to allow robots to extract items from inventories visibly.

--- api/buildcraft/api/transport/pipe/IFlowFluid.java
+++ api/buildcraft/api/transport/pipe/IFlowFluid.java
@@ -11,12 +11,14 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 
 import buildcraft.api.core.IFluidFilter;
 import buildcraft.api.core.IFluidHandlerAdv;
+import buildcraft.api.transport.pluggable.PipePluggable;
 
 public interface IFlowFluid {
     /** @param millibuckets
      * @param from
      * @param filter The fluidstack that the extracted fluid must match, or null for any fluid.
      * @return The fluidstack extracted and inserted into the pipe. */
+    @Nullable
     FluidStack tryExtractFluid(int millibuckets, EnumFacing from, FluidStack filter);
 
     /** Advanced version of {@link #tryExtractFluid(int, EnumFacing, FluidStack)}. Note that this only works for
@@ -34,7 +36,18 @@ public interface IFlowFluid {
      * different fluid type.
      * 
      * @param from The side that the fluid should *not* go in, or null if the fluid may flow in any direction.
-     * 
      * @return The amount of fluid that was accepted, or 0 if no fluid was accepted. */
     int insertFluidsForce(FluidStack fluid, @Nullable EnumFacing from, boolean simulate);
+
+    /** Tries to extract fluids directly from the pipe. NOTE: This is intended for {@link PipeBehaviour} and
+     * {@link PipePluggable} implementors ONLY! This will result in very buggy behaviour if external tiles try to use
+     * this!
+     * 
+     * @param min The minimum amount of fluid to extract. If less than this amount is in the given center then nothing
+     *            will be extracted.
+     * @param section The section to extract from. Null means the center.
+     * @param simulate
+     * @return */
+    @Nullable
+    FluidStack extractFluidsForce(int min, int max, @Nullable EnumFacing section, boolean simulate);
 }
